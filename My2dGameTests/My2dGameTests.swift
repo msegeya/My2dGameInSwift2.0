16df7f17263c389c21c57afd9dc466d70bbe7c79
Karol Kedziora <Karol@pc103.home>
2015-03-06 22:25:31 +0100

    Initial Commit

---
+++ My2dGameTests/My2dGameTests.swift
@@ -0,0 +1,36 @@
+//
+//  My2dGameTests.swift
+//  My2dGameTests
+//
+//  Created by Karol Kedziora on 06.03.2015.
+//  Copyright (c) 2015 Karol Kedziora. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class My2dGameTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
